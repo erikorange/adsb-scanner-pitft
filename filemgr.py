550fe3339d1b562149f45272503067a7bfbc2ecf
erikorange <erikorange@gmail.com>
2020-10-29 16:02:49 -0400

    added close() logic

--- filemgr.py
+++ filemgr.py
@@ -1,11 +1,19 @@
 
 class FileMgr:
 
+    def __init__(self):
+        self.__isOpen = False
+
     def createFile(self, filename):
         self.__handle = open(filename, "w")
+        self.__isOpen = True
 
     def writeToFile(self, txt):
         self.__handle.write(txt + "\n")
 
     def closeFile(self):
-        self.__handle.close()
+        if (self.__isOpen):
+            self.__handle.close()
+            self.__isOpen = False
+        else:
+            return
